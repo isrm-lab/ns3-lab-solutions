a661a1b4479714b3fd939b8bb38e87f277261b42
vladtraista <vladtp91@gmail.com>
2020-04-03 20:28:44 +0300

    Better organize lab6 into tasks

--- lab6/ex4.sh
+++ lab6/ex4.sh
@@ -2,9 +2,5 @@
 
 for wifiManagerType in Aarf Minstrel Ideal
 do
-	for tries in 1 4 7
-	do
-		echo "Running tcp_${wifiManagerType}_${tries}.txt"
-		./waf --run "lab5 --apManager=ns3::"$wifiManagerType"WifiManager --tries=$tries --isTcp=true" | tail -n +7 | head -n -8 | cut -d ' ' -f2,3 > tcp_${wifiManagerType}_${tries}.txt
-	done
+	./waf --run "lab5 --apManager=ns3::"$wifiManagerType"WifiManager --isTcp=true" | tail -n +7 | head -n -8 | cut -d ' ' -f2,3 > tcp_${wifiManagerType}.txt
 done
