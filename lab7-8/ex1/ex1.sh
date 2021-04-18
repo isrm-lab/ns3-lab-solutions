bd0a2d01a33c1ea997839663685dc5ed9393b975
Vlad Traista-Popescu <vladtp91@gmail.com>
2021-04-18 11:29:42 +0300

    Update ex1.sh

--- lab7-8/ex1/ex1.sh
+++ lab7-8/ex1/ex1.sh
@@ -6,7 +6,7 @@ for nr in 4 6 7 20 40
 do
 	for cw in 3 7 15 31 63 127 255 511 1023 2047 4095
 	do
-		stats=$(./waf --run "lab6-7-cw --ns=$nr --nd=$nr --minCw=$cw --maxCw=$cw --pcap=false" | tail -n 1)
+		stats=$(./waf --run "lab7-8-cw --ns=$nr --nd=$nr --minCw=$cw --maxCw=$cw --pcap=false" | tail -n 1)
 		echo "$nr $cw $stats" >> cw_stats.txt
 	done
 done
